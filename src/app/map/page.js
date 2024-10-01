05fcfe1817dea560aa3b7ad00615c20685ad410d
Will England <willengland@u.boisestate.edu>
2024-10-01 11:41:04 -0600

    Directory Update with main page update

---
+++ src/app/map/page.js
@@ -0,0 +1,9 @@
+export default function Schedule() {
+    return (
+        <div>
+            <h1>Maps</h1>
+            <ol>
+            </ol>
+        </div>
+    );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -4,9 +4,26 @@ import styles from "./page.module.css"
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.headers}>This is the home page.</h1>
-      <Link href="/schedule">Click here to go to the schedule page.</Link>
-    </div>
+      <div>
+          <h1 className={styles.headers}>This is the home page.</h1>
+          <Link href="/schedule">
+              Click here to go to the schedule page.
+          </Link>
+          <div> {/* New line for this link */}
+              <Link href="/main">
+                  Click here to go to the main page.
+              </Link>
+          </div>
+          <div> {/* New line for this link */}
+              <Link href="/map">
+                  Click here to go to the map page.
+              </Link>
+          </div>
+          <div> {/* New line for this link */}
+              <Link href="/vendors">
+                  Click here to go to the vendors page.
+              </Link>
+          </div>
+      </div>
   );
 }
