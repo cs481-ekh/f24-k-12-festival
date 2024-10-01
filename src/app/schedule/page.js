217f573c8263a98b883d7ddffc239d148d13d791
Anthony Malang <anthonymalang@u.boisestate.edu>
2024-10-01 10:38:35 -0600

    Created schedule page to demonstrate how the linking works to the main page

---
+++ src/app/schedule/page.js
@@ -0,0 +1,11 @@
+export default function Schedule() {
+    return (
+        <div>
+            <h1>Schedule</h1>
+            <ol>
+                <li>Vendor 1</li>
+                <li>Vendor 2</li>
+            </ol>
+        </div>
+    );
+}
