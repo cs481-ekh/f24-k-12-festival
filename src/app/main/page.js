dbd13ec0c1837d6eaddb39087983c437ca8b110e
WillEngl <111773025+WillEngl@users.noreply.github.com>
2024-10-01 11:49:11 -0600

    Directory Update with main page update (#67)

---
+++ src/app/main/page.js
@@ -0,0 +1,10 @@
+export default function Schedule() {
+    return (
+        <div>
+            <h1>Main Page</h1>
+            <ol>
+                <li>Boise State</li>
+            </ol>
+        </div>
+    );
+}
