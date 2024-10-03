3a29ade495790d61246c7c3484065df19c5d0507
RACHELLAWRENCE <RACHELLAWRENCE@u.boisestate.edu>
2024-10-03 13:34:32 -0600

    figured out why it got messed up, i changed about.module.css to about.css last minute thinking it wouldnt change anything i switched it back to about.module.css and everything is back to normal

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,33 @@
 import Link from "next/link";
-import "./globals.css"
-import styles from "./page.module.css"
+import "./globals.css";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
-      <div>
-          <h1 className={styles.headers}>This is the home page.</h1>
-          <Link href="/schedule">
-              Click here to go to the schedule page.
-          </Link>
-          <div> {/* New line for this link */}
-              <Link href="/map">
-                  Click here to go to the map page.
-              </Link>
-          </div>
-          <div> {/* New line for this link */}
-              <Link href="/vendors">
-                  Click here to go to the vendors page.
-              </Link>
-          </div>
+    <div>
+      <h1 className={styles.headers}>This is the home page.</h1>
+
+      <Link href="/schedule">
+        Click here to go to the schedule page.
+      </Link>
+
+      <div> {/* New line for this link */}
+        <Link href="/map">
+          Click here to go to the map page.
+        </Link>
       </div>
+
+      <div> {/* New line for this link */}
+        <Link href="/vendors">
+          Click here to go to the vendors page.
+        </Link>
+      </div>
+
+      <div> {/* New line for this link */}
+        <Link href="/about">
+          Click here to go to the about page.
+        </Link>
+      </div>
+    </div>
   );
 }
