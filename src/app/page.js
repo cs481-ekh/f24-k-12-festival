f4ef220e176f1ceefd8ea3e3d842408fe51ec9e4
RACHELLAWRENCE <RACHELLAWRENCE@u.boisestate.edu>
2024-10-03 20:25:30 -0600

    #73 did some basic homepage work, wanted to set some sort of format for the rest of the pages. feel free to change.

--- src/app/page.js
+++ src/app/page.js
@@ -1,33 +1,47 @@
-import Link from "next/link";
-import "./globals.css";
-import styles from "./page.module.css";
+// pages/index.js (or wherever your Home component is located)
+
+import Link from 'next/link';
+import Image from 'next/image';
+import boiseStateLogo from '/public/images/Boise_State_University_logo.png'; // Adjust path as necessary
+import './globals.css';
+import styles from './page.module.css';
 
 export default function Home() {
   return (
     <div>
-      <h1 className={styles.headers}>This is the home page.</h1>
+      {/* Header Section */}
+      <header className={styles.header}>
+        <div className={styles.logoContainer}>
+          <Link href="/">
+            <Image
+              src={boiseStateLogo}
+              alt="Boise State University Logo"
+              width={180}
+              height={55}
+            />
+          </Link>
+        </div>
+        <nav className={styles.navbar}>
+          <Link href="/schedule">
+            <button className={styles.navButton}>Schedule</button>
+          </Link>
+          <Link href="/map">
+            <button className={styles.navButton}>Map</button>
+          </Link>
+          <Link href="/vendors">
+            <button className={styles.navButton}>Vendors</button>
+          </Link>
+          <Link href="/about">
+            <button className={styles.navButton}>About</button>
+          </Link>
+        </nav>
+      </header>
 
-      <Link href="/schedule">
-        Click here to go to the schedule page.
-      </Link>
-
-      <div> {/* New line for this link */}
-        <Link href="/map">
-          Click here to go to the map page.
-        </Link>
-      </div>
-
-      <div> {/* New line for this link */}
-        <Link href="/vendors">
-          Click here to go to the vendors page.
-        </Link>
-      </div>
-
-      <div> {/* New line for this link */}
-        <Link href="/about">
-          Click here to go to the about page.
-        </Link>
-      </div>
+      {/* Main Content */}
+      <main>
+        <h1 className={styles.headers}>K-12 Engineering Festival</h1>
+        {/* Rest of your content */}
+      </main>
     </div>
   );
 }
