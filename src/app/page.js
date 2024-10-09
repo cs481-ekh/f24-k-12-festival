ae971966310442aa06059daa1ec15836b34ce415
Anthony M <72099814+junebugtony@users.noreply.github.com>
2024-10-09 10:47:09 -0600

    installed TailwindCSS (#86)
    Closes #85

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div>
-      <h1 className={styles.headers}>This is the home page.</h1>
+      <h1 className="text-3xl text-center">This is the home page.</h1>
 
       <Link href="/schedule">
         Click here to go to the schedule page.
