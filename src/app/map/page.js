35b52e374f54949a3485e6c999b6cb1ef89cf5fe
Will England <willengland@u.boisestate.edu>
2024-10-22 09:26:37 -0600

    Fixed Maps

--- src/app/map/page.js
+++ src/app/map/page.js
@@ -5,31 +5,32 @@ import React, { useState } from 'react';
 function MyComponent() {
   const [selectedBuilding, setSelectedBuilding] = useState("");
 
+  // Define your base path if the app is deployed under a subdirectory
+  const basePath = '/k-12-festival'; // Make sure this matches your actual base path
+  
   // Object mapping building names to their corresponding map images
   const buildingMaps = {
-    "029-ENGR": "/029-ENGR.svg",
-    "029-ENGR-1": "/029_ENGR_1.svg",
-    "100-HML-1": "/100_HML-1.svg",
-    "100-HML-2": "/100_HML-2.svg",
-    "100-MCE-1": "/100-MCE-1st.svg",
-    "100-MCE-2": "/100-MCE-2.svg",
-    "100-MCE-3": "/100-MCE-3.svg",
-    "100-MCE-4": "/100-MCE-4.svg",
-    "393-MCMR-1": "/393-MCMR-1.svg",
-    "393-MCMR-3": "/393-MCMR-3.svg",
-
-    "025-BGYM-1": "/025-BGYM-1.svg",
-    "025-BGYM-2": "/025-BGYM-2.svg",
-    "025-BGYM-B": "/025-BGYM-B.svg",
-    "025-BGYM-D": "/025-BGYM-D.svg",
-    "025-BGYM-R": "/025-BGYM-R.svg",
-    "027-ALBR-1": "/027-ALBR-1.svg",
-    "027-ALBR-2": "/027-ALBR-2.svg",
-    "032-SUBFIRST": "/032-SUBFIRST.svg",
-    "032-SUBSECOND": "/032-SUBSECOND.svg",
-    "032-SUBTHIRD": "/032-SUBTHIRD.svg",
-    "267-ERB-1": "/267-ERB-1.svg",
-    "349-CVA-FIRST": "/349-CVA-FIRST.svg"
+    "029-ENGR-1": `${basePath}/025-BGYM-1.svg`,
+    "100-HML-1": `${basePath}/100_HML-1.svg`,
+    "100-HML-2": `${basePath}/100_HML-2.svg`,
+    "100-MCE-1": `${basePath}/100-MCE-1st.svg`,
+    "100-MCE-2": `${basePath}/100-MCE-2.svg`,
+    "100-MCE-3": `${basePath}/100-MCE-3.svg`,
+    "100-MCE-4": `${basePath}/100-MCE-4.svg`,
+    "393-MCMR-1": `${basePath}/393-MCMR-1.svg`,
+    "393-MCMR-3": `${basePath}/393-MCMR-3.svg`,
+    "025-BGYM-1": `${basePath}/025-BGYM-1.svg`,
+    "025-BGYM-2": `${basePath}/025-BGYM-2.svg`,
+    "025-BGYM-B": `${basePath}/025-BGYM-B.svg`,
+    "025-BGYM-D": `${basePath}/025-BGYM-D.svg`,
+    "025-BGYM-R": `${basePath}/025-BGYM-R.svg`,
+    "027-ALBR-1": `${basePath}/027-ALBR-1.svg`,
+    "027-ALBR-2": `${basePath}/027-ALBR-2.svg`,
+    "032-SUBFIRST": `${basePath}/032-SUBFIRST.svg`,
+    "032-SUBSECOND": `${basePath}/032-SUBSECOND.svg`,
+    "032-SUBTHIRD": `${basePath}/032-SUBTHIRD.svg`,
+    "267-ERB-1": `${basePath}/267-ERB-1.svg`,
+    "349-CVA-FIRST": `${basePath}/349-CVA-FIRST.svg`
   };
 
   // Handler to update selected building
