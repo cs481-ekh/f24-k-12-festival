f4a61b96988ed333a27ee69d71641f8e50f56433
Anthony Malang <anthonymalang@u.boisestate.edu>
2024-10-22 11:14:42 -0600

    adjust SQL statement for rooms

--- src/app/api/vendors/filterOptions/route.js
+++ src/app/api/vendors/filterOptions/route.js
@@ -6,7 +6,7 @@ export async function GET() {
 
   const ageOptions = await db.all('SELECT DISTINCT age_range FROM vendors');
   const buildingOptions = await db.all('SELECT DISTINCT building FROM vendors');
-  const roomOptions = await db.all('SELECT DISTINCT room FROM vendors');
+  const roomOptions = await db.all('SELECT DISTINCT room FROM vendors WHERE room <> ""');
   const vendorNameOptions = await db.all('SELECT DISTINCT vendor_name FROM vendors');
 
   return NextResponse.json({
