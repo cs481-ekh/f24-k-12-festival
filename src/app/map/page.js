fad00f53ac1bbb3efebe17eb0b3f2831493267bb
Will England <willengland@u.boisestate.edu>
2024-10-27 09:57:01 -0600

    Updated maps naming and removed unused maps

--- src/app/map/page.js
+++ src/app/map/page.js
@@ -7,27 +7,30 @@ function MyComponent() {
   
   // Object mapping building names to their corresponding map images
   const buildingMaps = {
-    "029-ENGR-1": `${process.env.NEXT_PUBLIC_BASE_PATH}/025-BGYM-1.svg`,
-    "100-HML-1": `${process.env.NEXT_PUBLIC_BASE_PATH}/100_HML-1.svg`,
-    "100-HML-2": `${process.env.NEXT_PUBLIC_BASE_PATH}/100_HML-2.svg`,
-    "100-MCE-1": `${process.env.NEXT_PUBLIC_BASE_PATH}/100-MCE-1st.svg`,
-    "100-MCE-2": `${process.env.NEXT_PUBLIC_BASE_PATH}/100-MCE-2.svg`,
-    "100-MCE-3": `${process.env.NEXT_PUBLIC_BASE_PATH}/100-MCE-3.svg`,
-    "100-MCE-4": `${process.env.NEXT_PUBLIC_BASE_PATH}/100-MCE-4.svg`,
-    "393-MCMR-1": `${process.env.NEXT_PUBLIC_BASE_PATH}/393-MCMR-1.svg`,
-    "393-MCMR-3": `${process.env.NEXT_PUBLIC_BASE_PATH}/393-MCMR-3.svg`,
-    "025-BGYM-1": `${process.env.NEXT_PUBLIC_BASE_PATH}/025-BGYM-1.svg`,
-    "025-BGYM-2": `${process.env.NEXT_PUBLIC_BASE_PATH}/025-BGYM-2.svg`,
-    "025-BGYM-B": `${process.env.NEXT_PUBLIC_BASE_PATH}/025-BGYM-B.svg`,
-    "025-BGYM-D": `${process.env.NEXT_PUBLIC_BASE_PATH}/025-BGYM-D.svg`,
-    "025-BGYM-R": `${process.env.NEXT_PUBLIC_BASE_PATH}/025-BGYM-R.svg`,
-    "027-ALBR-1": `${process.env.NEXT_PUBLIC_BASE_PATH}/027-ALBR-1.svg`,
-    "027-ALBR-2": `${process.env.NEXT_PUBLIC_BASE_PATH}/027-ALBR-2.svg`,
-    "032-SUBFIRST": `${process.env.NEXT_PUBLIC_BASE_PATH}/032-SUBFIRST.svg`,
-    "032-SUBSECOND": `${process.env.NEXT_PUBLIC_BASE_PATH}/032-SUBSECOND.svg`,
-    "032-SUBTHIRD": `${process.env.NEXT_PUBLIC_BASE_PATH}/032-SUBTHIRD.svg`,
-    "267-ERB-1": `${process.env.NEXT_PUBLIC_BASE_PATH}/267-ERB-1.svg`,
-    "349-CVA-FIRST": `${process.env.NEXT_PUBLIC_BASE_PATH}/349-CVA-FIRST.svg`
+    "ENGR-First": `${process.env.NEXT_PUBLIC_BASE_PATH}/ENGR-First.svg`,
+    "ENGR-Second": `${process.env.NEXT_PUBLIC_BASE_PATH}/ENGR-Second.svg`,
+    "ENGR-Third": `${process.env.NEXT_PUBLIC_BASE_PATH}/ENGR-Third.svg`,
+    "HMCV-First": `${process.env.NEXT_PUBLIC_BASE_PATH}/HMCV-First.svg`,
+    "HMCV-Second": `${process.env.NEXT_PUBLIC_BASE_PATH}/HMCV-Second.svg`,
+    // "100-MCE-1": `${process.env.NEXT_PUBLIC_BASE_PATH}/100-MCE-1st.svg`,
+    // "100-MCE-2": `${process.env.NEXT_PUBLIC_BASE_PATH}/100-MCE-2.svg`,
+    // "100-MCE-3": `${process.env.NEXT_PUBLIC_BASE_PATH}/100-MCE-3.svg`,
+    // "100-MCE-4": `${process.env.NEXT_PUBLIC_BASE_PATH}/100-MCE-4.svg`,
+    "MCMR-First": `${process.env.NEXT_PUBLIC_BASE_PATH}/MCMR-First.svg`,
+    "MCMR-Second": `${process.env.NEXT_PUBLIC_BASE_PATH}/MCMR-Second.svg`,
+    "MCMR-Third": `${process.env.NEXT_PUBLIC_BASE_PATH}/MCMR-Third.svg`,
+    "Kinesiology-Building-First": `${process.env.NEXT_PUBLIC_BASE_PATH}/Kinesiology-Building-First.svg`,
+    "Kinesiology-Building-Second": `${process.env.NEXT_PUBLIC_BASE_PATH}/Kinesiology-Building-Second.svg`,
+    // "025-BGYM-B": `${process.env.NEXT_PUBLIC_BASE_PATH}/025-BGYM-B.svg`,
+    // "025-BGYM-D": `${process.env.NEXT_PUBLIC_BASE_PATH}/025-BGYM-D.svg`,
+    // "025-BGYM-R": `${process.env.NEXT_PUBLIC_BASE_PATH}/025-BGYM-R.svg`,
+    "ALBR-First": `${process.env.NEXT_PUBLIC_BASE_PATH}/ALBR-First.svg`,
+    "ALBR-Second": `${process.env.NEXT_PUBLIC_BASE_PATH}/ALBR-Second.svg`,
+    "Sub-First-Floor": `${process.env.NEXT_PUBLIC_BASE_PATH}/Sub-First-Floor.svg`,
+    "Sub-Second-Floor": `${process.env.NEXT_PUBLIC_BASE_PATH}/Sub-Second-Floor.svg`,
+    "Sub-Third-Floor": `${process.env.NEXT_PUBLIC_BASE_PATH}/Sub-Third-Floor.svg`,
+    "ERB": `${process.env.NEXT_PUBLIC_BASE_PATH}/ERB.svg`,
+    "CVA": `${process.env.NEXT_PUBLIC_BASE_PATH}/CVA.svg`
   };
 
   // Handler to update selected building
@@ -41,26 +44,25 @@ function MyComponent() {
       <label htmlFor="building-select">Choose a building:</label>
       <select id="building-select" onChange={handleBuildingChange}>
         <option value="">Select a building</option>
-        <option value="029-ENGR-1">029-ENGR-1</option>
-        <option value="100-HML-1">100-HML-1</option>
-        <option value="100-HML-2">100-HML-2</option>
-        <option value="100-MCE-1">100-MCE-1</option>
-        <option value="100-MCE-2">100-MCE-2</option>
-        <option value="393-MCMR-1">393-MCMR-1</option>
-        <option value="393-MCMR-3">393-MCMR-3</option>
-
-        <option value="025-BGYM-1">025-BGYM-1</option>
-        <option value="025-BGYM-2">025-BGYM-2</option>
-        <option value="025-BGYM-B">025-BGYM-B</option>
-        <option value="025-BGYM-D">025-BGYM-D</option>
-        <option value="025-BGYM-R">025-BGYM-R</option>
-        <option value="027-ALBR-1">027-ALBR-1</option>
-        <option value="027-ALBR-2">027-ALBR-2</option>
-        <option value="032-SUBFIRST">032-SUBFIRST</option>
-        <option value="032-SUBSECOND">032-SUBSECOND</option>
-        <option value="032-SUBTHIRD">032-SUBTHIRD</option>
-        <option value="267-ERB-1">267-ERB-1</option>
-        <option value="349-CVA-FIRST">349-CVA-FIRST</option>
+        <option value="ENGR-First">Ruch Engineering Building First Floor</option>
+        <option value="ENGR-Second">Ruch Engineering Building Second Floor</option>
+        <option value="ENGR-Third">Ruch Engineering Building Third Floor</option>
+        <option value="HMCV-First">Harry Morrison Civil Engineering First Floor</option>
+        <option value="HMCV-Second">Harry Morrison Civil Engineering Second Floor</option>
+        {/* <option value="100-MCE-1">100-MCE-1</option>
+        <option value="100-MCE-2">100-MCE-2</option> */}
+        <option value="MCMR-First">Micron Center for Materials Research First Floor</option>
+        <option value="MCMR-Second">Micron Center for Materials Research Second Floor</option>
+        <option value="MCMR-Third">Micron Center for Materials Research Third Floor</option>
+        <option value="Kinesiology-Building-First">Kinesiology Building First Floor</option>
+        <option value="Kinesiology-Building-Second">Kinesiology Building Second Floor</option>
+        <option value="ALBR-First">Albertson Library First</option>
+        <option value="ALBR-Second">Albertson Library Second</option>
+        <option value="Sub-First-Floor">Student Union Building First Floor</option>
+        <option value="Sub-Second-Floor">Student Union Building Second Floor</option>
+        <option value="Sub-Third-Floor">Student Union Building-Third-Floor</option>
+        <option value="ERB">Environmental Research Building First & Second</option>
+        <option value="CVA">Center for the Visual Arts</option>
       </select>
 
       {/* Conditionally render the selected map */}
