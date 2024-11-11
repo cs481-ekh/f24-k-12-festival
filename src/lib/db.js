e95fb760200366012afbb7d8739a20bf0e327a6f
Josh Martin <joshmartin174@u.boisestate.edu>
2024-11-11 13:11:14 -0700

    local -> production

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,20 +1,20 @@
 import sqlite3 from 'sqlite3'
 import { open } from 'sqlite'
 
-// Use this for production
-
-// export async function openDB() {
-//     return open({
-//         filename: '/app/data/vendors.db',
-//         driver: sqlite3.Database,
-//     });
-// }
-
-// Use this for local development
+//Use this for production
 
 export async function openDB() {
     return open({
-        filename: './vendors.db',
+        filename: '/app/data/vendors.db',
         driver: sqlite3.Database,
     });
 }
+
+// Use this for local development
+
+// export async function openDB() {
+//     return open({
+//         filename: './vendors.db',
+//         driver: sqlite3.Database,
+//     });
+// }
