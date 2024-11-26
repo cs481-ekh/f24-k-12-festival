127b3031e8043953b9f98f8da9867ee080c49267
Anthony Malang <anthonymalang@u.boisestate.edu>
2024-11-25 17:36:23 -0700

    added sponsor section + attendance form

--- src/app/page.js
+++ src/app/page.js
@@ -9,6 +9,7 @@ import {
 } from "@/components/ui/carousel";
 import Image from "next/image";
 import Autoplay from "embla-carousel-autoplay";
+import Link from "next/link";
 
 const previousFestivalImages = [
   { src: `${process.env.NEXT_PUBLIC_BASE_PATH}/PreviousFestival1.jpg`, alt: "Previous Festival Image 1" },
@@ -50,7 +51,7 @@ export default function Home() {
           </p>
 
           <Carousel
-            className="w-full sm:w-3/4 lg:w-3/4 xl:w-1/2 mx-auto relative"
+            className="w-full sm:w-3/4 lg:max-w-1/2 mx-auto relative"
             opts={{
               align: "center",
               loop: true,
@@ -66,14 +67,13 @@ export default function Home() {
           >
             <CarouselContent className="flex gap-4">
               {previousFestivalImages.map((image, index) => (
-                <CarouselItem key={index} className="relative flex-shrink-0 w-full">
+                <CarouselItem key={index} className="relative flex-shrink-0 w-full h-[calc(100vw*3/4)] sm:h-[calc(75vw*3/4)] lg:h-[calc(50vw*3/4)]">
                   <Image
                     src={image.src}
                     alt={image.alt}
                     width={1280}
-                    height={720}
-                    sizes="(max-width: 768px) 100vw, 50vw"
-                    className="w-full h-auto object-cover rounded-lg shadow-lg"
+                    height={960}
+                    className="w-full h-full object-cover rounded-lg shadow-lg"
                   />
                 </CarouselItem>
               ))}
@@ -83,6 +83,42 @@ export default function Home() {
           </Carousel>
         </div>
       </section>
+
+      <section className="mt-12 w-full">
+        <div className="text-center mb-8">
+          <h2 className="text-2xl md:text-3xl font-semibold text-gray-800 mb-6">
+            Meet our STEM Outreach Partners!
+          </h2>
+          <p className="text-lg md:text-xl text-gray-600 mb-6">
+            The Annual Engineering and Science Festival would not be possible without the support of our community supporters.
+            <br></br>
+            Discover our sponsors and learn more about them by clicking the button below.
+          </p>
+          <Link href="https://www.boisestate.edu/coen-mssc-old/outreach-recruitment/engineering-and-science-festival-sponsors/">
+            <button className="bg-bsu-blue text-white px-6 py-3 rounded hover:bg-orange-500 transition-colors">
+              Learn more
+            </button>
+          </Link>
+        </div>
+      </section>
+
+      <section className="mt-12 w-full">
+        <div className="text-center mb-8">
+          <h1 className="text-3xl md:text-4xl font-bold text-gray-800 mb-4 text-center">
+            Join the Festival!
+          </h1>
+          <p className="text-lg md:text-xl text-gray-600 mb-6">
+            Sign up to participate in this year's Engineering and Science Festival. Fill out the form below to get started!
+          </p>
+          <div className="w-full h-[220vh] sm:w-3/4 lg:max-w-1/2 mx-auto">
+            <iframe
+              src="https://app.smartsheet.com/b/form/b46202be87ce478ab9ff37ad1fb9c1a1"
+              className="w-full h-full rounded-lg shadow-lg border-2 border-gray-300"
+              allowFullScreen
+            />
+          </div>
+        </div>
+      </section>
     </div>
-  );
+  )
 }
